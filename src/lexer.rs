a75aea2890982916d38fe1c7e92e331e17c5bec3
RubixDev <silas.groh@t-online.de>
2022-01-29 16:48:24 +0100

    Support windows line endings

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,7 +6,7 @@ const LETTERS_AND_UNDERSCORE: [char; 53] = ['A', 'a', 'B', 'b', 'C', 'c', 'D',
     'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k',
     'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S',
     's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '_'];
-const SPACES: [char; 2] = [' ', '\t'];
+const SPACES: [char; 3] = [' ', '\t', '\r'];
 
 const SINGLE_CHARS: [char; 13] = ['(', ')', '{', '}', '?', ':', '|', '&', ',', '\n', ';', '%', '\\'];
 const OPTIONAL_EQ_CHARS: [char; 6] = ['=', '!', '<', '>', '+', '-'];
